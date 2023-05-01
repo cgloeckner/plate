13ec866d0ae81bfbafb1df9c4b5499ce98d67d30
cgloeckner <cgloeckner@freenet.de>
2023-05-01 16:31:26 +0200

    cleanup asteroids

--- main.py
+++ main.py
@@ -3,34 +3,7 @@ import moderngl
 import random
 
 from core import app, resources, particles, render
-
-
-import array
-
-
-class AsteroidsField(render.RenderBatch):
-    def __init__(self, context: moderngl.Context, cache: resources.Cache, field_size: int):
-        super().__init__(context, cache, field_size)
-
-        self.tex = cache.get_svg('data/sprites/asteroid.svg', scale=10)
-        self.tex.filter = moderngl.NEAREST, moderngl.NEAREST
-
-        self.velocity = array.array('f')
-
-    def add_asteroid(self, center: pygame.math.Vector2, scale: float, velocity: pygame.math.Vector2) -> None:
-        s = render.Sprite(self.tex)
-        s.center = center
-        s.rotation = random.uniform(0.0, 360.0)
-        s.scale *= scale / 10
-        self.append(s)
-        self.velocity.extend(velocity.xy)
-
-    def update(self, elapsed_ms: int) -> None:
-        delta = elapsed_ms * 0.01
-        for index in range(len(self)):
-            self.data[index * len(render.Offset) + render.Offset.POS_X] += self.velocity[index * 2] * delta
-            self.data[index * len(render.Offset) + render.Offset.POS_Y] += self.velocity[index * 2 + 1] * delta
-            self.data[index * len(render.Offset) + render.Offset.ROTATION] += delta
+import asteroids
 
 
 class StarField:
@@ -84,7 +57,7 @@ class DemoState(app.State):
         self.starfield.sprite.clip.w *= 10
         self.starfield.sprite.clip.h *= 10
 
-        self.asteroids = AsteroidsField(self.engine.context, self.cache, 1000)
+        self.asteroids = asteroids.AsteroidsField(self.engine.context, self.cache, 1000)
         for _ in range(1000):
             x = random.randrange(0, 1600 * 10)
             y = random.randrange(0, 900 * 10)
